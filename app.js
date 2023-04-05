162b980a1f3f4988f8bfdc53b6d6673df3e5ec03
SaiBalaji2001 <gadisaibalaji2001@gmail.com>
2023-04-05 17:04:16 -0500

    Added selector file

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var drinksRouter = require('./routes/Drink');
 var boardRouter = require('./routes/board');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Drink', drinksRouter);
 app.use('/board', boardRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler'
 app.use(function(req, res, next) {
